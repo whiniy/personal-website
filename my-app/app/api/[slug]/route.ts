c9446d2bd0ce73e9d53ab5cc484a4f6c1a5ef968
whiniy <winnie.t835@gmail.com>
2024-01-11 22:26:17 -0800

    ignore

--- my-app/app/api/[slug]/route.ts
+++ my-app/app/api/[slug]/route.ts
@@ -9,22 +9,7 @@ type IParams = {
 		}
 }
 
-/* 
-	In order to use params, you need to have a request parameter before
 
-	The reason why we do { params }, is to destructure, the object, meaning,
-	it allows us to obtain the individual properties within the "IParams" 
-	object directly and conveniently, 
-	such as the `params` property.
-
-	If we didn't do this, to obtain slug would look messy,
-	ex.
-	const slug = params.params.slug
-
-	There are more ways to destructure this, but that is up to you to find out
-	lol.
-
- */
 export async function GET(req: NextRequest, { params }: IParams) {
     await connectDB() // function from db.ts before
 		const { slug } = params // another destructure

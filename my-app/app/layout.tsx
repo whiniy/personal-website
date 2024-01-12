b70fbe6656b4a82d21591a5556f3e8e988cf09e2
whiniy <winnie.t835@gmail.com>
2024-01-12 00:02:58 -0800

    .

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <Navbar/>
         {children}
         </body>
+        <Footer/>
     </html>
   )
 }

6cda9305246f3f76fa96a7e6904f68604904899f
whiniy <winnie.t835@gmail.com>
2024-01-18 19:51:00 -0800

    trying to fix vercel

--- h4i/milestone2/next.config.js
+++ h4i/milestone2/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+/*module.exports = nextConfig*/
+module.exports = {
+    webpack: (config) => {
+      config.resolve.fallback = {
+        "mongodb-client-encryption": false ,
+        "aws4": false
+      };
+  
+      return config;
+    }
+}
--- h4i/milestone2/src/app/blog/page.tsx
+++ h4i/milestone2/src/app/blog/page.tsx
@@ -32,7 +32,7 @@ export default async function Blog() {
                 <h1>My Blogs!</h1>
                 <br></br>
                 <div>
-                    {blogs.map(blog => 
+                    {blogs.map((blog: any) => 
                     <div>
                         <BlogPrev
                             slug={blog.slug} 

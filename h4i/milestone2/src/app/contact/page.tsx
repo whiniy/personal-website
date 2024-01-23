2a0c9796801bdadf74da8f588ecefc868b2e5dbc
whiniy <winnie.t835@gmail.com>
2024-01-23 06:15:31 -0800

    done

--- h4i/milestone2/src/app/contact/page.tsx
+++ h4i/milestone2/src/app/contact/page.tsx
@@ -1,48 +1,12 @@
 import React from "react";
 import style from "../contact/page.module.css"
+import EmailForm from "@/components/contactForm/contactForm";
 
 export default function Contact() {
     return (
-        <div className = {style.container}>
-            <h1 className= {style.page_title}>Contact Me!</h1>
-            <form>
-                <div>
-                    <div>
-                        <label htmlFor="name" className = {style.label}>Name</label>
-                    </div>
-                    
-                    <div>
-                        <input type="text" className = {style.input} required/>
-                    </div>
-                </div>
-                
-                <br></br>
-
-                <div>
-                    <div>
-                        <label htmlFor="email" className = {style.label}>Email</label>
-                    </div>
-                    
-                    <div>
-                        <input type="email" className = {style.input} required/>
-                    </div>
-                </div>
-
-                <br></br>
-
-                <div>
-                    <div>
-                        <label htmlFor="message" className = {style.label}>Message</label>
-                    </div>
-                    
-                    <div>
-                        <textarea className = {style.message}></textarea>
-                    </div>
-                </div>
-                <div>
-                    <input type="submit" className = {style.submit} required/>
-                </div>
-            </form>
+        <div className = {style.container} id="contactMe">
+            <h1 className= {style.page_title} >Contact Me!</h1>
+            <EmailForm/>
         </div>
     );
 }
---
+++ h4i/milestone2/src/components/contactForm/contactForm.tsx
@@ -0,0 +1,81 @@
+"use client"
+import React, { useState } from 'react'
+import emailjs from '@emailjs/browser';
+import style from "../contactForm/contactForm.module.css"
+
+const EmailForm = () => {
+    
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const serviceId = 'service_347w0rr';
+        const templateId = 'contact_form';
+        const publicKey = 'giLU5GdzMHGkdj1Jc';
+        
+        const templateParams = {
+            from_name: name,
+            user_email: email,
+            to_name: 'Winnie Trinh',
+            message: message,
+        };
+
+        
+        emailjs.send(serviceId, templateId, templateParams, publicKey)
+            .then((response) => {
+                console.log('Email sent successfully!', response.status, response.text);
+                alert('Success!');
+                setName('');
+                setEmail('');
+                setMessage('');
+                
+                
+            })
+            .catch((error) => {
+                console.error('Error sending email:', error);
+                alert('Oh No! ' + JSON.stringify(error));
+            });
+    }
+    
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className={style.divContainer}>
+                <h3 className={style.label}>Name</h3>
+                <input className={style.input}
+                    type = "text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+
+            <div className={style.divContainer}>
+                <h3 className={style.label}>Email</h3>
+                <input className={style.input}
+                    type = "email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+
+            <div className={style.divContainer}>
+                <h3 className={style.label}>Message</h3>
+                <textarea className={style.message}
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                >
+                </textarea>
+            </div>
+            <div>
+            <button type="submit" className={style.submit}>Send</button>
+            </div>
+
+        </form>
+
+    )
+
+}
+
+export default EmailForm
